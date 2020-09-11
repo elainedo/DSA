e0c7b5c119b9fb50e15475ff5501cedc1a8253ff
elainedo <elainedo827@gmail.com>
2020-09-11 17:26:11 -0500

    improve syntax for rotate 2d array  problem

--- Array/Rotate2DArray.py
+++ Array/Rotate2DArray.py
@@ -10,4 +10,17 @@ def rotateMatrix(matrix):
     
     return matrix
 
+# Improve syntax
+def rotateMatrix2(matrix):
+    n = len(matrix)
+    for i in range(len(matrix)//2):
+        for j in range(i, n-i-1):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[~j][i]
+            matrix[~j][i] = matrix[~i][~j]
+            matrix[~i][~j] = matrix[j][~i]
+            matrix[j][~i] = temp
+    
+    return matrix
+
 print(rotateMatrix([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]))
