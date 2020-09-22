d99fe7510f64361d55a74c7adaa73aad07d2dc5e
elainedo <elainedo827@gmail.com>
2020-09-21 22:43:05 -0500

    add car pooling problem

--- Array/CarPooling.py
+++ Array/CarPooling.py
@@ -1,22 +1,12 @@
 """
-You are driving a vehicle that has capacity empty seats initially available for passengers.  
-The vehicle only drives east (ie. it cannot turn around and drive west.)
 
-Given a list of trips, trip[i] = [num_passengers, start_location, end_location] contains 
-information about the i-th trip: the number of passengers that must be picked up, and the 
-locations to pick them up and drop them off.  The locations are given as the number of 
-kilometers due east from your vehicle's initial location.
+There is a car pooling system that picks up and drops off passengers but
+every vehicle in this system can only drive in one direction. Each vehicle 
+has a capacity (empty seats) of passengers.
 
-Return true if and only if it is possible to pick up and drop off all passengers for all 
-the given trips. 
+Given a list of trips where trip[i] = [num_passenger, start, end], return if 
+you can pick up and drop off passengers at locations specified. 
 
-Example 1:
-Input: trips = [[2,1,5],[3,3,7]], capacity = 4
-Output: false
-
-Example 2:
-Input: trips = [[2,1,5],[3,3,7]], capacity = 5
-Output: true
 """
 
 def carPooling(trips, capacity):
